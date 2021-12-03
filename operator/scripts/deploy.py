bd228fb6d1ef6df0c317110bc109a0dcded18b9a
matthias zimmermann <zimmermann@xbrain.ch>
2021-12-03 23:26:25 +0100

    fix/improve brownie deploy script

--- operator/scripts/deploy.py
+++ operator/scripts/deploy.py
@@ -11,54 +11,54 @@ from brownie import (
 from scripts.helpful_scripts import get_account
 from scripts.helpful_scripts import get_contract
 
-# name of imported account to deploy operator contract
-NODE_ACCOUNT = 'node_account'
-KEYSTORE_FILE = './{}.json'
+# use node account to deploy operator contract
+CHAINLINK_DIR = '/opt/chainlinknode' 
+KEYSTORE_FILE = '{}/keystore.json'.format(CHAINLINK_DIR)
+PASSWORD_FILE = '{}/password'.format(CHAINLINK_DIR)
 
-PASSWORD_ENV = 'NODE_ACCOUNT_PW'
-KEYSTORE_ENV = 'NODE_ACCOUNT_KEYSTORE'
+# output file with address of deployed operator
 OPERATOR_ADDRESS_FILE = './operator.address'
 
-def get_account_local():
-    if os.environ[KEYSTORE_ENV] and os.environ[PASSWORD_ENV]:
-        keystore_file = os.environ[KEYSTORE_ENV]
-        password = os.environ[PASSWORD_ENV]
 
-        return accounts.load(filename=keystore_file, password=password) 
+def get_node_account():
+    if os.path.exists(KEYSTORE_FILE):
+        with open(PASSWORD_FILE) as f:
+            password = f.readline().strip()
+        
+        return accounts.load(filename=KEYSTORE_FILE, password=password) 
 
     return get_account()
 
 
 def main():
-    account = get_account_local()
+    account = get_node_account()
     link_contract = get_contract('link_token')
 
     print('link address {}'.format(link_contract.address))
     print('deployer address {}'.format(account.address))
     print('owner address {}'.format(account.address))
 
+    # unsure funding of the chainlink node account
     i = 0
     while account.balance() == 0.0:
-        print('waiting for funding of {}'.format(account.address))
-        time.sleep(5)
+        print('{} waiting for funding of {}'.format(i, account.address))
+        time.sleep(10)
         i += 1
 
-        if i > 2:
-            accounts[0].transfer(account, "0.01 ether")
-
+    # deploy operator contract with node account as contract owner
+    print('deploying operator contract')
     operator = ChainlinkOperator.deploy(
         link_contract.address, 
         account.address, 
         { 'from': account.address })
     
-    print('contract address {}'.format(operator.address))
+    print('operator contract address {}'.format(operator.address))
+    print('writing contract address to file {}'.format(OPERATOR_ADDRESS_FILE))
     with open(OPERATOR_ADDRESS_FILE, 'w') as f:
         f.write(operator.address)
 
-    print('getAuthorizedSenders(): {}'.format(operator.getAuthorizedSenders()))
-
-    # needs to be called by chainlink node after deploying operator to add itself
-    # to allowed sender list. 
+    # call using chainlink node account after deploying operator to add 
+    # node account to allowed sender list. 
     # fulfillOracleRequest and fulfillOracleRequest2 may only be called from addresses registered as authorized senders
     print('add contract owner address to authorized senders (= accounts from chainlink nodes that are allowed to call )')
     authorized_senders = [account.address]
